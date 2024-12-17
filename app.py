b8244c89f348f4ea20da1926a5511ba7ede0509c
Ludivine.RB <ludivine.raby@gmail.com>
2024-12-17 23:37:43 +0100

    restoring board

--- app.py
+++ app.py
@@ -43,28 +43,77 @@ screen.blit(title, (screen_width/2-100, screen_height/2))
 def pursuit_board():
     center = (screen_width/2-450, screen_height/2)
     radius = 400
+    radius_50 = 340
+    radius_100 = 280
+    radius_150= 220
+    radius_200 = 160
+    radius_250 = 100
     
-    cases= Game.create_board(36)
+    game = Game(36)
+    cases= Game.create_board(game)
 
     for i in range(len(cases)):
         angle = (2 * math.pi / len(cases)) * i
         x = center[0] + int(radius * math.cos(angle))
         y = center[1] + int(radius * math.sin(angle))
 
-        # if i % 8 == 0:
-        #     #case_color = random.choice(liste_of_colours)
-        #     pygame.draw.circle(screen, green, (x, y), 30)
-        if cases[i] == 'Bases de données':
-            pygame.draw.circle(screen, blue, (x, y), 20)
+        if i % 6 == 0:
+            if cases[i]== "Bases de données":
+                pygame.draw.circle(screen, blue, (x, y), 30)
+            if cases[i]== "Langages de programmation":
+                pygame.draw.circle(screen, green, (x, y), 30)
+            if cases[i]== "Ligne de commandes":
+                pygame.draw.circle(screen, pink, (x, y), 30)
+            if cases[i]== "Actualités IA":
+                pygame.draw.circle(screen, orange, (x, y), 30)
+            if cases[i]== "Autres catégories":
+                pygame.draw.circle(screen, yellow, (x, y), 30)
+            if cases[i]== "DevOps":
+                pygame.draw.circle(screen, purple, (x, y), 30)
+            
+            # #initialize picture
+            cheese = pygame.image.load("media/cheese_slice.png")
+            cheese = pygame.transform.scale(cheese, (30,30))
+            screen.blit(cheese, (x-15,y-15)) #a rescale et décaler
 
-        elif i % 8 == 1:
-            #case_color = random.choice(liste_of_colours)
-            pygame.draw.circle(screen, light_grey, (x, y), 20)
+            
+            angle = (2 * math.pi / len(cases)) * i
+            x1 = center[0] + int(radius_250 * math.cos(angle))
+            y1 = center[1] + int(radius_250 * math.sin(angle))
+            pygame.draw.circle(screen, light_grey, (x1, y1), 28)
+            x2 = center[0] + int(radius_200 * math.cos(angle))
+            y2 = center[1] + int(radius_200 * math.sin(angle))
+            pygame.draw.circle(screen, light_grey, (x2, y2), 26)
+            x3 = center[0] + int(radius_150 * math.cos(angle))
+            y3 = center[1] + int(radius_150 * math.sin(angle))
+            pygame.draw.circle(screen, light_grey, (x3, y3), 24)
+            x4 = center[0] + int(radius_100 * math.cos(angle))
+            y4 = center[1] + int(radius_100 * math.sin(angle))
+            pygame.draw.circle(screen, light_grey, (x4, y4), 22)
+            x5 = center[0] + int(radius_50 * math.cos(angle))
+            y5 = center[1] + int(radius_50 * math.sin(angle))
+            pygame.draw.circle(screen, light_grey, (x5, y5), 20)
+                
+            # if cases[i] == 'Bases de données':
+            #     pygame.draw.circle(screen, blue, (x, y), 20)
         else:
-            pygame.draw.circle(screen, pink, (x, y), 20)
-    screen.blit(sub_title, (screen_width/2-250, screen_height/2-500))
-    pygame.draw.circle(screen, black, center, 50)
-    pygame.draw.circle(screen, light_grey, center, 48)
+
+            if cases[i]== "Bases de données":
+                pygame.draw.circle(screen, blue, (x, y), 20)
+            if cases[i]== "Langages de programmation":
+                pygame.draw.circle(screen, green, (x, y), 20)
+            if cases[i]== "Ligne de commandes":
+                pygame.draw.circle(screen, pink, (x, y), 20)
+            if cases[i]== "Actualités IA":
+                pygame.draw.circle(screen, orange, (x, y), 20)
+            if cases[i]== "Autres catégories":
+                pygame.draw.circle(screen, yellow, (x, y), 20)
+            if cases[i]== "DevOps":
+                pygame.draw.circle(screen, purple, (x, y), 20)
+
+    pygame.draw.circle(screen, black, center, 60)
+    pygame.draw.circle(screen, light_grey, center, 58)
+    screen.blit(sub_title, (screen_width/2-100, screen_height/2-500))
 
 
 #start the display
