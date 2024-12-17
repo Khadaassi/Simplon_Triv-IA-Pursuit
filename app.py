9b122d308629283b8b5a7ba8c59cf14e765970cd
Ludivine.RB <ludivine.raby@gmail.com>
2024-12-17 11:34:45 +0100

    modification pygame

--- app.py
+++ app.py
@@ -28,17 +28,19 @@ screen_height = screen.get_height()
 
 #initialize text
 smallfont = pygame.font.SysFont('Corbel',35)
+middlefont = pygame.font.SysFont('Corbel',100)
 bigfont = pygame.font.SysFont('Corbel',200)
 text_start = smallfont.render('Start' , True , black)
 text_game_choice=bigfont.render('Choose your game', True, black)
 text_solo = smallfont.render('Solo' , True , black)
 text_multiplayer = smallfont.render('Multiplayer', True, black)
-title = bigfont.render('Triv-IA-pursuite', True, black)
+title = bigfont.render('Triv-IA-pursuit', True, black)
+sub_title=middlefont.render('Triv-IA-pursuit', True, black)
 screen.blit(title, (screen_width/2-100, screen_height/2))
 
 #initialize the circle grids
 def pursuit_board():
-    center = (screen_width/2, screen_height/2)
+    center = (screen_width/2-450, screen_height/2)
     radius = 400
     cases = 48
 
@@ -55,9 +57,9 @@ def pursuit_board():
             pygame.draw.circle(screen, light_grey, (x, y), 20)
         else:
             pygame.draw.circle(screen, pink, (x, y), 20)
-
+    screen.blit(sub_title, (screen_width/2-250, screen_height/2-500))
     pygame.draw.circle(screen, black, center, 50)
-    pygame.draw.circle(screen, light_grey, center, 45)
+    pygame.draw.circle(screen, light_grey, center, 48)
 
 
 #start the display
